e053da365817c7c2a23936c7c73fafea66d6ea59
vikornienko <vikornienko76@gmail.com>
2026-05-14 17:13:15 +0500

    refactor: rewrite aiogramstart.ps1
    Файл aiogramstart.ps1 переписывается с использованием функций.

--- aiogramstart.ps1
+++ aiogramstart.ps1
@@ -1,169 +1,195 @@
 # Скрипт для старта проекта на aiogram и автоматической установки зависимостей.
-# Версия: 0.0.1
+# Версия: 0.0.2
 # Автор В.И. Корниенко vikornienko76@gmail.com
 # Скрипт автоматизирует старт проекта telegram bot на aiogram3.
 
-# Запрашивает имя проекта и версию python
-$projectName = Read-Host "Write project name: "
-$pythonVersion = Read-Host "Write python version: "
+
+function Add-ProjectParameters {
+    # Запрашивает имя проекта и версию Python
+    [CmdletBinding()]
+    param ()
+    $projectName = Read-Host "Write project name: "
+    $pythonVersion = Read-Host "Write python version: "
+
+    # Возвращает хеш-таблицу с параметрами
+    @{
+        ProjectName = $projectName
+        PythonVersion = $pythonVersion
+    }
+    
+}
+
 
 # Создает папку проекта и переходит с нее.
-New-Item -ItemType Directory -Name $projectName | Out-Null
-Set-Location $projectName
+# New-Item -ItemType Directory -Name $projectName | Out-Null
+# Set-Location $projectName
 
-uv init . --bare --python $pythonVersion
-uv sync
-uv add aiogram aiosqlite sqlalchemy python-decouple
+# uv init . --bare --python $pythonVersion
+# uv sync
+# uv add aiogram aiosqlite sqlalchemy python-decouple
 
 # Создает файл .gitignore
-@"
-# Byte-compiled / optimized / DLL files
-__pycache__/
-*.py[cod]
-*$py.class
+# @"
+# # Byte-compiled / optimized / DLL files
+# __pycache__/
+# *.py[cod]
+# *$py.class
 
-# C extensions
-*.so
+# # C extensions
+# *.so
 
-# Distribution / packaging
-.Python
-build/
-develop-eggs/
-dist/
-downloads/
-eggs/
-.eggs/
-lib/
-lib64/
-parts/
-sdist/
-var/
-wheels/
-share/python-wheels/
-*.egg-info/
-.installed.cfg
-*.egg
-MANIFEST
+# # Distribution / packaging
+# .Python
+# build/
+# develop-eggs/
+# dist/
+# downloads/
+# eggs/
+# .eggs/
+# lib/
+# lib64/
+# parts/
+# sdist/
+# var/
+# wheels/
+# share/python-wheels/
+# *.egg-info/
+# .installed.cfg
+# *.egg
+# MANIFEST
 
-# PyInstaller
-*.manifest
-*.spec
+# # PyInstaller
+# *.manifest
+# *.spec
 
-# Installer logs
-pip-log.txt
-pip-delete-this-directory.txt
+# # Installer logs
+# pip-log.txt
+# pip-delete-this-directory.txt
 
-# Logs
-/logs/
+# # Logs
+# /logs/
 
-# Unit test / coverage reports
-htmlcov/
-.tox/
-.nox/
-.coverage
-.coverage.*
-.cache
-nosetests.xml
-coverage.xml
-*.cover
-*.py,cover
-.hypothesis/
-.pytest_cache/
-cover/
+# # Unit test / coverage reports
+# htmlcov/
+# .tox/
+# .nox/
+# .coverage
+# .coverage.*
+# .cache
+# nosetests.xml
+# coverage.xml
+# *.cover
+# *.py,cover
+# .hypothesis/
+# .pytest_cache/
+# cover/
 
-# Translations
-*.mo
-*.pot
+# # Translations
+# *.mo
+# *.pot
 
-# Django stuff:
-*.log
-local_settings.py
-db.sqlite3
-db.sqlite3-journal
+# # Django stuff:
+# *.log
+# local_settings.py
+# db.sqlite3
+# db.sqlite3-journal
 
-# Flask stuff:
-instance/
-.webassets-cache
+# # Flask stuff:
+# instance/
+# .webassets-cache
 
-# Scrapy stuff:
-.scrapy
+# # Scrapy stuff:
+# .scrapy
 
-# Sphinx documentation
-docs/_build/
+# # Sphinx documentation
+# docs/_build/
 
-# PyBuilder
-.pybuilder/
-target/
+# # PyBuilder
+# .pybuilder/
+# target/
 
-# Jupyter Notebook
-.ipynb_checkpoints
+# # Jupyter Notebook
+# .ipynb_checkpoints
 
-# IPython
-profile_default/
-ipython_config.py
+# # IPython
+# profile_default/
+# ipython_config.py
 
-# PEP 582; used by e.g. github.com/David-OConnor/pyflow
-__pypackages__/
+# # PEP 582; used by e.g. github.com/David-OConnor/pyflow
+# __pypackages__/
 
-# Celery stuff
-celerybeat-schedule
-celerybeat.pid
+# # Celery stuff
+# celerybeat-schedule
+# celerybeat.pid
 
-# SageMath parsed files
-*.sage.py
+# # SageMath parsed files
+# *.sage.py
 
-# Environments
-.env
-.venv
-env/
-venv/
-ENV/
-env.bak/
-venv.bak/
+# # Environments
+# .env
+# .venv
+# env/
+# venv/
+# ENV/
+# env.bak/
+# venv.bak/
 
-# Spyder project settings
-.spyderproject
-.spyproject
+# # Spyder project settings
+# .spyderproject
+# .spyproject
 
-# Rope project settings
-.ropeproject
+# # Rope project settings
+# .ropeproject
 
-# mkdocs documentation
-/site
+# # mkdocs documentation
+# /site
 
-# mypy
-.mypy_cache/
-.dmypy.json
-dmypy.json
+# # mypy
+# .mypy_cache/
+# .dmypy.json
+# dmypy.json
 
-# Pyre type checker
-.pyre/
+# # Pyre type checker
+# .pyre/
 
-# pytype static type analyzer
-.pytype/
+# # pytype static type analyzer
+# .pytype/
 
-# Cython debug symbols
-cython_debug/
+# # Cython debug symbols
+# cython_debug/
 
-# Editors
-.vscode/
-.idea/
+# # Editors
+# .vscode/
+# .idea/
 
-# Vagrant
-.vagrant/
+# # Vagrant
+# .vagrant/
 
-# Mac/OSX
-.DS_Store
+# # Mac/OSX
+# .DS_Store
 
-# Windows
-Thumbs.db
+# # Windows
+# Thumbs.db
 
-# pyenv
-.python-version
+# # pyenv
+# .python-version
 
-# Project specific
-functionalTesting/geckodriver.log
-functionalTesting/swb/geckodriver
-/other_files/
-/getinfoapp/files/
-"@ | Out-File -FilePath .\.gitignore -Encoding utf8
+# # Project specific
+# functionalTesting/geckodriver.log
+# functionalTesting/swb/geckodriver
+# /other_files/
+# /getinfoapp/files/
+# "@ | Out-File -FilePath .\.gitignore -Encoding utf8
+
+function Main {
+    # Основная логика скрипта
+    $params = Add-ProjectParameters
+
+    if (-not $params.ProjectName -or -not $params.PythonVersion) {
+        Write-Error "Project name and Python version are required."
+        return
+    }
+    
+}
+# Запуск скрипта
+Main
